bfb3f4b4a1d93379fc6ac1c4e52e09392d634d6a
lemosjp01 <33187777+lemosjp01@users.noreply.github.com>
2018-01-25 23:19:53 -0200

    second commit

--- express-app/index.js
+++ express-app/index.js
@@ -2,13 +2,43 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', function(req, res){
+app.use((req, res, next) => {
+    console.log('Incluído um primeiro Middleware que intercepta as chamadas de APIs')
+    next()
+}, (req, res, next) => {
+    console.log('Incluído um segundo Middleware que intercepta as chamadas de APIs')
+    next()
+})
+
+//API com resposta normal
+app.get('/', (req, res) => {
     console.log('Nossa primeira API')
     res.send('Welcome to Express.')
 })
 
+
+//Retorna os dados em JSON
+app.get('/json/resp', (req, res) => {
+    var obj = {
+        date: Date.now(),
+        resposta: 'Chamada bem sucedida.'
+    }
+    res.send(obj)
+})
+
+//API enviando parametros
+//Utilizado para os métodos DELETE e PUT para remoção ou atualização.
+
+app.get('/parametro/:userId', (req, res) => {
+    console.log(req, params)
+    var obj = {
+        idRecebido: req.params.userId,
+    }
+    res.send(obj)
+})
+
 const PORT = 3000
 const HOST = 'localhost'
-app.listen(PORT, function(){
+app.listen(PORT, () => {
     console.log('Server in port: 3000')
 })
